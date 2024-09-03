cb649f185dc2b61b2780bf62e58e4e5497dd87b2
errob37 <roberge.er@gmail.com>
2024-09-03 19:27:24 -0400

    fix: Fix script

--- src/main/createDay.sh
+++ src/main/createDay.sh
@@ -1,6 +1,6 @@
 #bash
 
-echo  "package days.impl.$2\n\n\nimport days.impl.AdventOfCodeDayImpl\n\nclass Day$1 : AdventOfCodeDayImpl($1, 0L, 0L) {\n\n override fun partOne(input: List<String>): Long { return 0L}\noverride fun partTwo(input: List<String>): Long {return 0L}  }" > ./kotlin/days/impl/$2/Day$1.kt;
+echo  "package days.impl.$2\n\n\nimport days.impl.AdventOfCodeDayImpl\n\nclass Day$1 : AdventOfCodeDayImpl($2, $1, 0L, 0L) {\n\n override fun partOne(input: List<String>): Long { return 0L}\noverride fun partTwo(input: List<String>): Long {return 0L}  }" > ./kotlin/days/impl/$2/Day$1.kt;
 
 touch ./resources/input/$2/day-$1.txt;
 touch ./resources/test/$2/day-$1-1.txt;
